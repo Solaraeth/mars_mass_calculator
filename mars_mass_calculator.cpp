2a6ff5e89925935b65e2f3d746061a25e035c2af
Khthonian <anansicorp@tuta.io>
2023-07-25 23:20:24 +0100

    Update mars_mass_calculator.cpp and README.md
    - Corrected indentation
    - Corrected grammatical errors

--- mars_mass_calculator.cpp
+++ mars_mass_calculator.cpp
@@ -1,22 +1,21 @@
-// Program that converts Earth mass into its Mars equivelant.
+// Program that converts Earth mass into its Mars equivalent.
 #include <iostream>
 
 int main() {
 
-// Define the variables for mass on earth and mars
-float mass_earth;
-float mass_mars;
+    // Define the variables for mass on Earth and Mars
+    float mass_earth;
+    float mass_mars;
 
-// Ask the user to input a weight value which is assigned to the variable mass_earth
-std::cout << "Earth weight (kg): ";
-std::cin >> mass_earth;
+    // Ask the user to input a weight value which is assigned to the variable mass_earth
+    std::cout << "Earth weight (kg): ";
+    std::cin >> mass_earth;
 
-// Convert inputed earth mass to mars mass and assign to variable mass_mars
-mass_mars = mass_earth / 2.66;
+    // Convert input Earth mass to Mars mass and assign to variable mass_mars
+    mass_mars = mass_earth / 2.66;
 
-// Print converted mars equivelant mass
-std::cout << mass_earth << "kg on Earth is equivelant to " << mass_mars << "kg on Mars.";
-return 0;
-
+    // Print converted Mars equivalent mass
+    std::cout << mass_earth << "kg on Earth is equivalant to " << mass_mars << "kg on Mars.";
+    return 0;
 
 }
