d54a47e77d7601b845a02e39ebef8e995d6edc3a
Khthonian <anansicorp@tuta.io>
2023-07-26 23:05:05 +0100

    Add user input validation

--- mars_mass_calculator.cpp
+++ mars_mass_calculator.cpp
@@ -7,15 +7,33 @@ int main() {
     float mass_earth;
     float mass_mars;
 
-    // Ask the user to input a weight value which is assigned to the variable mass_earth
-    std::cout << "Earth weight (kg): ";
-    std::cin >> mass_earth;
+    bool validInput = false;
+
+    // Validate the user input
+    while (!validInput) {
+        // Ask the user to input a weight value which is assigned to the variable mass_earth
+        std::cout << "Earth weight (kg): ";
+        std::string input;
+        std::cin >> input;
+
+        // Try to convert the input string to a float
+        try {
+            mass_earth = std::stof(input);
+            validInput = true;
+        }     
+        
+        // Catch exceptions and give the user a retry prompt
+        catch (const std::exception& e) {
+            std::cout << "Invalid input. Please enter a valid floating-point number.\n";
+        }
+    }
+    
 
     // Convert input Earth mass to Mars mass and assign to variable mass_mars
     mass_mars = mass_earth / 2.66;
 
     // Print converted Mars equivalent mass
-    std::cout << mass_earth << "kg on Earth is equivalant to " << mass_mars << "kg on Mars.";
+    std::cout << mass_earth << "kg on Earth is equivalant to " << mass_mars << "kg on Mars." << std::endl;
     return 0;
 
 }
